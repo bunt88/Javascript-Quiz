1875b10aa66eff7b247e4c1af764fce60c1d42df
unknown <bunt88@gmail.com>
2022-09-03 12:45:06 -0600

    Refactored questions code into an array

--- README.md
+++ README.md
@@ -7,9 +7,8 @@ you should be able to go from question to question until the quiz is complete
 
 When quiz is complete you should be able to save your score and compare with your peers.
 
-
-
 As a developer I need to create the functionality so that the user may accomplish what is expected.
 
+![](<assets%5CImages%5CScreenshot%20(8).png>)
 
-![](assets%5CImages%5CScreenshot%20(8).png)
+https://bunt88.github.io/Javascript-Quiz/
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,121 @@
 var start = document.getElementById("start");
 var begin = document.getElementById("begin");
 var timer = document.getElementById("time")
+
 // var time = questions.length * 10;
+
 var questionBoxEl = document.getElementById("question-box");
 var question = document.getElementById("question");
 var answer1 = document.getElementById("answer-1");
 var answer2 = document.getElementById("answer-2");
 var answer3 = document.getElementById("answer-3");
 var answer4 = document.getElementById("answer-4");
-var next = document.getElementById("next");
-var positiveresponse = document.getElementById("positiveresponse");
-var negativeresponse = document.getElementById("negativeresponse");
-var finsih = document.getElementById("finish-screen");
-var finalscore = document.getElementById("final-score");
-var submit = document.getElementById("submit");
-var highScores = document.getElementById("highscores")
-var correctanswer
 
+var next = document.getElementById("next");
+
+var positiveResponse = document.getElementById("positiveresponse");
+var negativeResponse = document.getElementById("negativeresponse");
+
+var finish = document.getElementById("finish-screen");
+var finalScore = document.getElementById("final-score");
+
+var submit = document.getElementById("submit");
+
+var highScores = document.getElementById("highscores")
+
+var correctAnswer;
+
+// NOTE: added a simple timer/countdown, questionRandom & questionNumber variable here to assign later in the beginQuiz() function
+
+var countDown;
+var questionRandom;
+var questionNumber;
+
+//TODO ---> Start by Creating an Array of Questions and Answers to work with
+// NOTE : you can name the questions variable whatever you'd like, but here's an example below:
+// NOTE : This is an array of objects. this will help us work in our functions when needing to reference different data points.
+
+// NOTICE --> we refactored these commented functions into an objects Array (questionsArray)
+// WHY >> Efficiency. By creating the questionsArray you've offloaded a lot of expense/work on a machine when it runs through the javascript
+
+
+var questionArray = [
+    {
+        question: "The condition in an if / else statement is enclosed with ___________.",
+        options: [
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "parentheses", correct: true },
+            { text: "square brackets", correct: false },
+        ]
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ___________.",
+        options: [
+            { text: "numbers and strings", correct: false },
+            { text: "other arrays", correct: false },
+            { text: "booleans", correct: false },
+            { text: "all of the above", correct: true },
+        ]
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        options: [
+            { text: "commas", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "quotes", correct: true },
+            { text: "parentheses", correct: false },
+        ]
+    }
+]
+
+//TODO --> beginquiz - in this function:
+// -> start timer,
+// -> render and randomize questions array,
+// -> select an answer,
+// -> go to next question
 
 function beginQuiz() {
-    // hide start screen and start timer
+    // hide start screen and start timer -> using the countDown variable created prior (line 31)
+
+    // TODO -> you can now assign your countDown variable and create a timer using the setInterval() method
+    // TODO --> Future ToDo -> create a function for clockTick();
+
+    countDown = setInterval(clocktick, 1000);
+    // timer.textContent = time;
+
+        //var timerId = setInterval(clockTick, 1000);
+    //  NOTE --> your original code was extremely close you could make this work how you have it,
+    //      you are on the perfect track. the best part about coding is theres always more than way to do something.
+
+
+     //TODO --> use questionRandom variable to randomize/sort your questionArray (hint: use .sort() and Math.random() methods)
+
+    questionRandom = questionArray.sort(() => Math.random() > .5 ? 1 : -1);
+
+       // NOTE --> within Math.random() is a ternary operator. Basically shorthand for conditional statements like 'if' and 'case' blocks
+    //example syntax:  condition ? exprIfTrue : exprIfFalse
+    // Here's a link to a good source on ternary operators: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+
+    //TODO -> use questionNumber variable to set current question number
+
+        // timer.textContent = time;
+        
+    questionNumber = 0;
+
+    //NOTE: This is great! you are hiding the start element by setting it's style visibility attribute to hidden.
 
     start.setAttribute("style", "visibility:hidden");
-    questionBoxEl.setAttribute("style", "visibility:visible")
+    questionBoxEl.setAttribute("style", "visibility:visible");
     // var timerId = setInterval(clockTick, 1000);
-    // timer.textContent = time;
 
     questionOne();
 }
 
-function questionOne() {
+//TODO -> refactor 'questionOne()' function for questionsArray.
+//        NOTE : consider renaming this function for clarity (i.e. nextQuestion)
+
+function nextQuestion() {
     if (answer3.onclick = correctAnswer()) {
         questionTwo();
     } else if (answer1.onclick = wrongAnswer()) {
@@ -52,70 +138,12 @@ function wrongAnswer() {
     next.setAttribute("style", "visibility:visible")
 }
 
-
-
-function questionTwo() {
-
-
-}
-
-function getQuestionTwo() {
-    question.setAttribute("question", "The condition in an if / else statement is enclosed with______.");
-    answer1.setAttribute("answer-1", "quotes");
-    answer2.setAttribute("answer-2", "curly brackets");
-    answer3.setAttribute("answer-3", "parentheses");
-    answer4.setAttribute("answer-4", "square brackets");
-
-    if (answer3.onclick = correctAnswer()) {
-        questionThree();
-    } else if (answer1.onclick = wrongAnswer()) {
-
-    } else if (answer2.onclick = wrongAnswer()) {
-
-    } else (answer4.onclick = wrongAnswer())
-
-}
-
-function getQuestionThree() {
-    question.setAttribute("question", "Arrays in JavaScript can be used to store _______.");
-    answer1.setAttribute("answer-1", "numbers and strings");
-    answer2.setAttribute("answer-2", "other arrays");
-    answer3.setAttribute("answer-3", "booleans");
-    answer4.setAttribute("answer-4", "all of the above");
-
-    if (answer4.onclick = correctAnswer()) {
-        questionFour();
-    } else if (answer1.onclick = wrongAnswer()) {
-
-    } else if (answer2.onclick = wrongAnswer()) {
-
-    } else (answer3.onclick = wrongAnswer())
-
-}
-
-function getQuestionFour() {
-    question.setAttribute("question", "String values must be enclosed within _____ when being assigned to variables.");
-    answer1.setAttribute("answer-1", "commas");
-    answer2.setAttribute("answer-2", "curly brackets");
-    answer3.setAttribute("answer-3", "quotes");
-    answer4.setAttribute("answer-4", "parentheses");
-
-    if (answer3.onclick = correctAnswer()) {
-        finishScreen();
-    } else if (answer1.onclick = wrongAnswer()) {
-
-    } else if (answer2.onclick = wrongAnswer()) {
-
-    } else (answer4.onclick = wrongAnswer())
-
-}
-
 function highScores() {
     highScores.setAttribute("style", "visibility:visible");
     questionBoxEl.setAttribute("style", "visibility:hidden")
 }
 
-
+// console.log(questionOne);
 console.log(questionOne);
 begin.onclick = beginQuiz;
 highScores.onclick = highScores();
